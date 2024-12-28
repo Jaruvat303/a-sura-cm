ca81b41dd5d7bbcc5f132755dcc3451c794c87a0
Jaruvat303 <jaruvat.see@gmail.com>
2024-12-28 11:58:12 +0700

    init project strapi

---
+++ src/api/specail-blog/routes/specail-blog.ts
@@ -0,0 +1,7 @@
+/**
+ * specail-blog router
+ */
+
+import { factories } from '@strapi/strapi';
+
+export default factories.createCoreRouter('api::specail-blog.specail-blog');
---
+++ src/api/specail-blog/services/specail-blog.ts
@@ -0,0 +1,7 @@
+/**
+ * specail-blog service
+ */
+
+import { factories } from '@strapi/strapi';
+
+export default factories.createCoreService('api::specail-blog.specail-blog');
